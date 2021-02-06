113d000c32e8ae187c0b8a2999e65c3b8a3fef74
Vitalii <vitalii.manchenko@rolique.io>
2021-02-06 21:59:22 +0200

    Fixed public path for docs.

--- webpack.docs.js
+++ webpack.docs.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'main.js',
-    publicPath: '/',
+    publicPath: 'https://bublikus.github.io/native-suggestions/',
   },
   plugins: [
     new HtmlWebpackPlugin({
